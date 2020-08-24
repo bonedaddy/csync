af34e0102d7e8066eca123796aba34774fb89110
postables <postables@rtradetechnologies.com>
2020-08-23 19:43:27 -0700

    tests: add more robust pool testing

--- tests/csync_test.c
+++ tests/csync_test.c
@@ -12,17 +12,36 @@
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-void *csync_cond_test_fn(void *data) {
-  csync_cond_t *cond = (csync_cond_t *)data;
-  csync_cond_wait(cond);
-  printf("done waiting\n");
-  return NULL;
-}
-
 typedef struct object_test {
   int a;
   int b;
 } object_test_t;
+
+
+void *csync_pool_test_fn(void *data) {
+  csync_pool_t *pool = (csync_pool_t *)data;
+
+  for (int i = 50; i < 60; i++) {
+    // this should create a new object
+    object_test_t *obj = (object_test_t *)csync_pool_get(pool);
+    assert(obj != NULL);
+    // sleep(0.01); this breaks testing
+    // set values
+    obj->a = i;
+    obj->b = i - 1;
+    csync_pool_put(pool, obj);
+  }
+
+  pthread_exit(NULL);
+}
+
+void *csync_cond_test_fn(void *data) {
+  csync_cond_t *cond = (csync_cond_t *)data;
+  csync_cond_wait(cond);
+  printf("done waiting\n");
+
+  pthread_exit(NULL);
+}
 
 void free_object_test(void *obj) {
   free((object_test_t *)obj);
@@ -61,15 +80,16 @@ void test_csync_cond_new(void **state) {
   pthread_t thread2;
 
   pthread_create(&thread1, NULL, csync_cond_test_fn, &cond);
-  pthread_detach(thread1);
 
   pthread_create(&thread2, NULL, csync_cond_test_fn, &cond);
-  pthread_detach(thread2);
 
-  sleep(2); // wait for threads to startup and shit
+  sleep(2);
 
   csync_cond_broadcast(&cond);
   csync_cond_signal(&cond);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
 }
 
 void test_csync_cond_new_null(void **state) {
@@ -79,15 +99,16 @@ void test_csync_cond_new_null(void **state) {
   pthread_t thread2;
   
   pthread_create(&thread1, NULL, csync_cond_test_fn, cond);
-  pthread_detach(thread1);
 
   pthread_create(&thread2, NULL, csync_cond_test_fn, cond);
-  pthread_detach(thread2);
 
   sleep(2); // wait for threads to startup and shit
 
   csync_cond_broadcast(cond);
   csync_cond_signal(cond);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
 }
 
 
@@ -124,6 +145,43 @@ void test_csync_pool(void **state) {
   csync_pool_put(pool, obj);
   csync_pool_put(pool, obj2);
   assert(pool->count == 2);
+
+  pthread_t thread1;
+  pthread_t thread2;
+
+  pthread_create(&thread1, NULL, csync_pool_test_fn, pool);
+  pthread_create(&thread2, NULL, csync_pool_test_fn, pool);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+
+  // ensure that we still only have 2 objects which means they were all reused
+  assert(pool->count == 2);
+
+
+  // remove objects so further tests create new objects
+  obj = (object_test_t *)csync_pool_get(pool);
+  obj2 = (object_test_t *)csync_pool_get(pool);
+
+  pthread_t thread3;
+  pthread_t thread4;
+
+  pthread_create(&thread1, NULL, csync_pool_test_fn, pool);
+  pthread_create(&thread2, NULL, csync_pool_test_fn, pool);
+  pthread_create(&thread3, NULL, csync_pool_test_fn, pool);
+  pthread_create(&thread4, NULL, csync_pool_test_fn, pool);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+  pthread_join(thread3, NULL);
+  pthread_join(thread4, NULL);
+
+  // put objects back
+  csync_pool_put(pool, obj);
+  csync_pool_put(pool, obj2);
+
+  assert(pool->count > 2);
+  printf("count: %u\n", pool->count);
 
   csync_pool_destroy(pool);
 }
